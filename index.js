3cbb62c3d84141cd30f666f52e00ab93e8526ccd
Helzady <91940556+Helzady@users.noreply.github.com>
2023-06-01 15:02:00 -0300

    add deploy global commads

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const fs = require('node:fs');
 const path =require('node:path');
-
 const client = new Discord.Client({ intents: [/*1, 512, 32768, 2, 128*/ 32767] });
-
 const glob = require('glob');
-
+client.commands = new Discord.Collection();
 const eventsPath = path.join(__dirname, './src/events');
 const eventFiles = glob.sync('**/*.js', { cwd: eventsPath });
+
+require('./src/handler')
 
 for (const file of eventFiles) {
 	const filePath = path.join(eventsPath, file);
@@ -18,38 +18,25 @@ for (const file of eventFiles) {
 	} else {
 		client.on(event.name, (...args) => event.execute(...args));
 	}
-}
-
-
-client.commands = new Discord.Collection();
+} 
 
 const foldersPath = path.join(__dirname, './src/slashCommands');
 const commandFolders = fs.readdirSync(foldersPath);
 
-client.once('ready', async () => {
-  for (const folder of commandFolders) {
-    const commandsPath = path.join(foldersPath, folder);
-    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-    for (const file of commandFiles) {
-      const filePath = path.join(commandsPath, file);
-      const command = require(filePath);
-      if ('data' in command && 'execute' in command) {
-        client.commands.set(command.data.name, command);
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] O comando em ${filePath} não possui uma propriedade "data" ou "execute" obrigatória.`);
+		}
+	}
+}
 
-        // Register the slash command in all guilds
-        try {
-          client.guilds.cache.forEach(async (guild) => {
-            await guild.commands.create(command.data);
-            
-          });
-        } catch (error) {
-          console.error(`Failed to register slash command "${command.data.name}" globally\n`, error);
-        }
-      } else {
-        console.log(`[WARNING] O comando em ${filePath} não possui uma propriedade "data" ou "execute" obrigatória.`);
-      }
-    }
-  }
-});
 
 client.login(process.env.token);
---
+++ src/handler/index.js
@@ -0,0 +1,51 @@
+const { REST, Routes } = require('discord.js'); 
+const fs = require('node:fs'); 
+const path = require('node:path');
+
+//const { clientId, token } = require('./config.json');
+
+require('dotenv').config();
+const clientId = process.env.clientId
+const token = process.env.token
+
+const commands = []; 
+
+const foldersPath = path.join(__dirname, '../../src/slashCommands');
+
+const commandFolders = fs.readdirSync(foldersPath); 
+
+for (const folder of commandFolders) { 
+	const commandsPath = path.join(foldersPath, folder); 
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js')); 
+
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		if ('data' in command && 'execute' in command) {
+			commands.push(command.data.toJSON()); 
+		} else {
+			console.log(`[WARNING] O comando em ${filePath} está faltando a propriedade "data" ou "execute".`);
+		}
+	}
+}
+
+
+const rest = new REST().setToken(token);
+
+
+(async () => {
+	try {
+		console.log(`Iniciando o registro global de ${commands.length} comandos (/) da aplicação.`);
+
+		
+		const data = await rest.put(
+			Routes.applicationCommands(clientId),
+			{ body: commands },
+		);
+
+		console.log(`Registrou global com sucesso ${data.length} comandos (/) da aplicação.`);
+	} catch (error) {
+		
+		console.error(error);
+	}
+})();
